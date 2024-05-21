d850ed99c66c4ea36a4df70c2755f4678b436de5
Evan Marmer <evanmarmer@gmail.com>
2024-05-21 08:52:29 -0600

    finish responsive nav

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,30 @@ function App() {
 
   return (
     <>
+      <header>
+        <img src='img/logo.png' alt='logo' className='logo' />
+
+        <input type='checkbox' id='check' />
+        <label for='check' class='icons'>
+        <i class='bx bx-menu' id='burger'></i>
+        <i class='bx bx-x' id='x'></i>
+        </label>
+
+        <nav className='navbar'>
+          <a href='#home'>Home</a>
+          <a href='#vehicle'>Vehicle Models</a>
+          <a href='#about'>About</a>
+          <a href='#testimonials'>Testimonials</a>
+          <a href='#contact'>Contact</a>
+          <div className='button-container'>
+            <button className='sign-in'>Sign In</button>
+            <button className='register'>Register</button>
+          </div>
+        </nav>
+
+
+      </header>
+      
     
     </>
   )
